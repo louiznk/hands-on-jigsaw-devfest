b1090e25495a79d0fd0fbd749013c0c2ad22d1c7
louis <louis.tournayre@zenika.com>
2018-10-12 13:48:51 +0200

    fix: add mvnw cmd, simplify pom and module-info

--- 2-sith/starwars-vertx-http-server/src/main/java/module-info.java
+++ 2-sith/starwars-vertx-http-server/src/main/java/module-info.java
@@ -1,7 +1,6 @@
 open module org.zenika.handson.jigsaw.http {
     requires java.logging;
     requires org.zenika.handson.jigsaw.api;
-    requires jdk.httpserver;
     requires vertx.core;
     requires vertx.web;
     uses org.zenika.handson.jigsaw.api.CharactersApi;
